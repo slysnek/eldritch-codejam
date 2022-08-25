d471cbc9a1046bf684cdf780f2d18bb32edfea5f
Kirill <bloodwingest@gmail.com>
2022-08-25 19:45:39 +0300

    feat: expanded layout, added some functions

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ import {brownCards,blueCards,greenCards} from "./data/mythicCards/index.js"
 import ancientsData from "./data/ancients.js";
 import difficulties from "./data/difficulties.js";
 
+let deckWrapper = document.querySelector('.deck-wrapper');
 
-console.log(brownCards[1].color);
+function initializeDecks(){
+    for (let i = 0; i < ancientsData.length; i++) {
+        let deck = document.createElement('div');
+        deck.classList.add('deck')
+        deck.style.backgroundImage = `url(./assets/Ancients/${ancientsData[i].cardFace})`
+        deckWrapper.appendChild(deck)
+    }
+}
+
+initializeDecks()
