8407af519288aacaf1165c86b765dfc19789d51f
Kirill <bloodwingest@gmail.com>
2022-08-28 20:01:16 +0300

    feat: added initializeDeck function

--- index.js
+++ index.js
@@ -1,16 +1,78 @@
 import {brownCards,blueCards,greenCards} from "./data/mythicCards/index.js"
 import ancientsData from "./data/ancients.js";
 import difficulties from "./data/difficulties.js";
+import cardsData from "./data/mythicCards/brown/index.js";
 
-let deckWrapper = document.querySelector('.deck-wrapper');
+const azathothDeck = document.querySelector('.azathoth')
+const cthulthuDeck = document.querySelector('.cthulthu')
+const iogsothothDeck = document.querySelector('.iogsothoth')
+const shobniggurathDeck = document.querySelector('.shobniggurath')
 
-function initializeDecks(){
-    for (let i = 0; i < ancientsData.length; i++) {
-        let deck = document.createElement('div');
-        deck.classList.add('deck')
-        deck.style.backgroundImage = `url(./assets/Ancients/${ancientsData[i].cardFace})`
-        deckWrapper.appendChild(deck)
+//собираем начальную колоду
+function initializeDeck(creatureName){
+    let stages = {
+        firstStage: [],
+        secondStage: [],
+        thirdStage: []
     }
+
+    let creature;
+    for (let i in ancientsData){
+        if (ancientsData[i].id === creatureName){
+            creature = ancientsData[i];
+        }
+    }
+    
+    //добавляем карты в первый стейдж
+    for(let i = 0; i < creature.firstStage.greenCards; i++){
+        stages.firstStage.push(greenCards[Math.floor(Math.random() * greenCards.length)])
+    }
+    for(let i = 0; i < creature.firstStage.blueCards; i++){
+        stages.firstStage.push(blueCards[Math.floor(Math.random() * blueCards.length)])
+    }
+    for(let i = 0; i < creature.firstStage.brownCards; i++){
+        stages.firstStage.push(brownCards[Math.floor(Math.random() * brownCards.length)])
+    }
+    //добавляем карты во второй стейдж
+    for(let i = 0; i < creature.secondStage.greenCards; i++){
+        stages.secondStage.push(greenCards[Math.floor(Math.random() * greenCards.length)])
+    }
+    for(let i = 0; i < creature.secondStage.blueCards; i++){
+        stages.secondStage.push(blueCards[Math.floor(Math.random() * blueCards.length)])
+    }
+    for(let i = 0; i < creature.secondStage.brownCards; i++){
+        stages.secondStage.push(brownCards[Math.floor(Math.random() * brownCards.length)])
+    }
+    //добавляем карты в третий стейдж
+    for(let i = 0; i < creature.thirdStage.greenCards; i++){
+        stages.thirdStage.push(greenCards[Math.floor(Math.random() * greenCards.length)])
+    }
+    for(let i = 0; i < creature.thirdStage.blueCards; i++){
+        stages.thirdStage.push(blueCards[Math.floor(Math.random() * blueCards.length)])
+    }
+    for(let i = 0; i < creature.thirdStage.brownCards; i++){
+        stages.thirdStage.push(brownCards[Math.floor(Math.random() * brownCards.length)])
+    }
+
+    console.log(stages.firstStage);
+    console.log(stages.secondStage);
+    console.log(stages.thirdStage);
+
 }
 
-initializeDecks()
+
+azathothDeck.addEventListener('click', () => {
+    initializeDeck('azathoth')
+})
+
+cthulthuDeck.addEventListener('click', () => {
+    initializeDeck('cthulhu')
+})
+
+iogsothothDeck.addEventListener('click', () => {
+    initializeDeck('iogSothoth')
+})
+
+shobniggurathDeck.addEventListener('click', () => {
+    initializeDeck('shubNiggurath')
+})
